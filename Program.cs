98b70ccb7cd25badb32f410bf771a5497789451b
2901shivam <shivamsingh201617@gmail.com>
2023-12-06 12:59:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1EndProject2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string op = "yes";
+            Teacher teacher1=new Teacher(1,"abc","12A","A2");
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter 1:Store Teacher data 2:Retrieve Teacher data 3:Update Teacher Data");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Enter Id:");
+                                int id = int.Parse(Console.ReadLine());
+                                Console.WriteLine("Enter Name:");
+                                string name = Console.ReadLine();
+                                Console.WriteLine("Enter Class:");
+                                string cls = Console.ReadLine();
+                                Console.WriteLine("Enter Section");
+                                string section = Console.ReadLine();
+                                
+                                teacher1.StoreData(id,name,cls,section);
+                                break;
+                            }
+                        case 2:
+                            {
+                               
+                                teacher1.RetrieveData();
+                                
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.WriteLine("Enter Id:");
+                                int id = int.Parse(Console.ReadLine());
+                                teacher1.UdateData(id);
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Invalid input");
+                                break;
+                            }
+                    }
+                    Console.WriteLine("Enter yes if you want to coninue");
+                    op = Console.ReadLine();
+
+                }catch(Exception e)
+                {
+                    Console.WriteLine("Error Occured"+e.Message);
+                }
+
+            }
+            
+            while (op=="yes");
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1EndProject2
+{
+    public class Teacher
+    {
+        int id;
+        string name;
+        string cls;
+        string section;
+
+        public Teacher(int id,string name,string cls,string section)
+        {
+            this.id = id;
+            this.name = name;
+            this.cls = cls;
+            this.section = section;
+
+        }
+
+        Teacher teacher;
+        string filePath = "C:\\Users\\shiva\\OneDrive\\Desktop\\File1.txt.txt";
+        List<Teacher>listOfTeacher=new List<Teacher>();
+        
+        
+        
+
+        public void StoreData(int id,string name ,string cls,string section)
+        {
+            teacher = new Teacher(id, name, cls, section);
+
+            listOfTeacher.Add(teacher);
+
+            Console.WriteLine("Data Added Successfully");
+
+            SaveToFile();
+        }
+
+        public void SaveToFile()
+        {
+            List<string> line = new List<string>();
+            foreach(var t in listOfTeacher)
+            {
+                line.Add($"Id:{t.id},Name:{t.name},Class:{t.cls},Section:{t.section}");
+
+            }
+            File.WriteAllLines(filePath, line);
+        }
+
+        public void RetrieveData()
+        {
+            string[] str = File.ReadAllLines(filePath);
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+        }
+
+        public void UdateData(int id)
+        {
+            foreach(var str in listOfTeacher)
+            {
+                if (str.id == id)
+                {
+                    Console.WriteLine("Enter the name for updation");
+                    string name = Console.ReadLine();
+                    str.name=name;
+                    Console.WriteLine("Enter the class for updation");
+                    string cls = Console.ReadLine();
+                    str.cls=cls;
+                    Console.WriteLine("Enter the section for updation");
+                    string section = Console.ReadLine();
+                    str.section=section;
+                    Console.WriteLine("Data Update Successfully");
+
+                }
+            }
+            SaveToFile();
+        }
+        
+    }
+}
